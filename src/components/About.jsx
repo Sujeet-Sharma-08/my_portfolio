3822e32545c21ea80d33fe5b7c8dfeef287f2f47
Sujeet Kumar Sharma <125545129+Sujeet-Sharma-08@users.noreply.github.com>
2024-02-04 18:24:26 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
                   Hey there,👋 I'm Sujeet Sharma a 21-Years-old <span className='text-blue-400'>computer science and engineering <br /></span> 
                       undergrad who is passionate about Software Development, <br />
                       Right now - I'm looking for an opportunity where I can use my skills and can contribute in large projects.
-                      Currentyl, I'am  focusing on mastering <span className='text-blue-400'>Data Structures and Algorithms </span > using <span className='text-blue-400'>C++ </span>
+                      Currently, I'am  focusing on mastering <span className='text-blue-400'>Data Structures and Algorithms </span > using <span className='text-blue-400'>C++ </span>
                       Language and deeping dive in the world of Full-Stack web development using <span className='text-blue-400'>MERN STACK. </span> 
                   </p>
 
